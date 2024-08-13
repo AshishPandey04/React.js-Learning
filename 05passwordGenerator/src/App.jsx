bac92246853ca9ea209ef143fc3e7d38afc6eeed
AshishPandey04 <0203ashish2004@gmail.com>
2024-08-14 03:33:16 +0530

    Password Generator

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -9,7 +8,7 @@ function App() {
   return (
     <>
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+        Password Generator
     </h1>
     </>
   )
