156f69d08787da7d393c9a14a05f1851b6315f00
AshishPandey04 <0203ashish2004@gmail.com>
2024-08-17 02:22:01 +0530

    currency converter

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import InputBox from './components/InputBox'
 
 import './App.css'
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
     <h1 className=' text-white text-3xl bg-blue-700 text-center py-1 my-4'>Currency Converter</h1>
+    <InputBox/>
     </>
   )
 }
