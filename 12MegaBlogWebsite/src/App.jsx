8e22225a7cc5953fa9124d7a993da4a5b7bfc3ee
AshishPandey04 <0203ashish2004@gmail.com>
2024-09-10 11:57:44 +0530

    environment variable and appwrite setup

--- 12MegaBlogWebsite/src/App.jsx
+++ 12MegaBlogWebsite/src/App.jsx
@@ -2,11 +2,12 @@
 import './App.css'
 
 function App() {
-  
+
+ console.log(import.meta.env.VITE_APPWRITE_URL) 
 
   return (
     <>
-    <h1>Ablog website with Appwrite</h1>
+    <h1>A blog website with Appwrite</h1>
     </>
   )
 }
